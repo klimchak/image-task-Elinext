290b823c0bc0287c98a633a5fb37f7280c8ce2f5
klim <shpektras@gmail.com>
2021-08-12 01:29:03 +0300

    make it better

--- src/app/app.interceptor.service.ts
+++ src/app/app.interceptor.service.ts
@@ -16,13 +16,24 @@ export class AppInterceptorService implements HttpInterceptor{
     }
     else {
       const request = req.clone({
+        body: req.body,
         headers: req.headers
+          .set('Accept', 'application/json, text/plain, */*')
+          .set('Accept-Encoding', 'gzip, deflate, br')
+          .set('Referer', 'https://task-img-elinext.herokuapp.com/')
           .set('Access-Control-Allow-Origin', 'https://task-img-elinext.herokuapp.com/')
           .set('Access-Control-Expose-Headers', 'ETag, Content-Type, Accept, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset')
           .set('Access-Control-Allow-Credentials', 'true')
           .set('Host', 'getpocket.com')
           .set('X-Accept', 'application/json')
-          .set('Content-Type', 'application/json; charset=UTF-8'),
+          .set('Origin', 'https://task-img-elinext.herokuapp.com')
+          .set('Connection', 'keep-alive')
+          .set('Sec-Fetch-Dest', 'empty')
+          .set('Sec-Fetch-Mode', 'no-cors')
+          .set('Sec-Fetch-Site', 'cross-site')
+          .set('Pragma', 'no-cache')
+          .set('Cache-Control', 'no-cache')
+          .set('TE',' trailers'),
         withCredentials: true,
       });
       console.log('cloneResp', request)
