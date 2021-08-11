5cb1dc9fd5c645203b302c907592c0f70f4faae1
klim <shpektras@gmail.com>
2021-08-11 23:37:38 +0300

    make it better

--- src/app/app.interceptor.service.ts
+++ src/app/app.interceptor.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from "@angular/core";
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
-import 'rxjs/add/observable/throw'
-import 'rxjs/add/operator/catch'
 import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Injectable()
