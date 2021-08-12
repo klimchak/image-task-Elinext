02c4ae8874ca7110cc1a5d5ba90b74d528683fa2
klim <shpektras@gmail.com>
2021-08-12 18:39:26 +0300

    make it better

--- src/app/app.apikey.ts
+++ src/app/app.apikey.ts
@@ -3,7 +3,7 @@ export const apikeys = {
   raindrop: '88296e43-c4fe-4881-8a98-cdb8a8a6e2a6', // delete
   flick: 'cb3e97553e235bd32b2fa493a878398a',
   raindropApi: {
-    redirect_uri: 'https://task-img-elinext.herokuapp.com/login',
+    redirect_uri: 'https://task-img-elinext.herokuapp.com',
     client_id: '611123ddcf708e9b6838133b',
     client_secret: 'b341602e-1268-4c7e-b210-70b795f027d9',
     grant_type: 'authorization_code'
