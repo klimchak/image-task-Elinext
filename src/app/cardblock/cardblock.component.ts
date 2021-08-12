32d6ae24a8ef53b9db2bc5acd88511e9ad173bca
klim <shpektras@gmail.com>
2021-08-12 13:40:21 +0300

    make it better

--- src/app/cardblock/cardblock.component.ts
+++ src/app/cardblock/cardblock.component.ts
@@ -64,6 +64,26 @@ export class CardblockComponent implements OnInit {
       this.req = response;
       console.log('getpocket.com', this.req)
     });
+    let httpWithoutInterceptor = new HttpClient(this.httpBackend);
+    httpWithoutInterceptor.get('https://api.raindrop.io/rest/v1/collections', {
+      headers: {
+        'Authorization': 'Bearer 65f8e71c-ecd5-4743-828d-a79718168070'
+      },
+      withCredentials: true
+    }).subscribe((response) => {
+      // this.req = response;
+      console.log('raindrop.io', response)
+    });
+
+    this.httpWithoutInterceptor.get('https://api.raindrop.io/rest/v1/collections', {
+      headers: {
+        'Authorization': 'Bearer 65f8e71c-ecd5-4743-828d-a79718168070'
+      },
+      withCredentials: true
+    }).subscribe((response) => {
+      // this.req = response;
+      console.log('raindrop.io', response)
+    });
     this.removeBookmark = true;
   }
 
