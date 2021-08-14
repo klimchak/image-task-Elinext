5c954c0b4ecd38a4430bef2ef698dad3fa20fa3e
klim <shpektras@gmail.com>
2021-08-15 02:00:59 +0300

    started writing tests

--- src/app/app.apikey.ts
+++ src/app/app.apikey.ts
@@ -1,10 +1,10 @@
 export const apikeys = {
-  google: '220448430227-7f66navqnc6jnfoss5cpn3rqddvhov0c.apps.googleusercontent.com',
-  flick: 'cb3e97553e235bd32b2fa493a878398a',
+  google: 'xxxxx',
+  flick: 'xxxxx',
   raindropApi: {
     redirect_uri: 'https://task-img-elinext.herokuapp.com',
-    client_id: '611123ddcf708e9b6838133b',
-    client_secret: 'b341602e-1268-4c7e-b210-70b795f027d9',
+    client_id: 'xxxxx',
+    client_secret: 'xxxxx-xxxxx-xxxxx-xxxxx-xxxxx',
     grant_type: 'authorization_code'
   }
 }
