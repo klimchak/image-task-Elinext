7da4c9e939788f46d9507ca961b02bc5eba2839c
klim <shpektras@gmail.com>
2021-08-14 12:53:43 +0300

    make it better

--- server.js
+++ server.js
@@ -8,38 +8,44 @@ const options = {
   target: 'http://devapi.saved.io',
   changeOrigin: true,
   ws: false,
-  logLevel: 'info',
+  logLevel: 'debug',
   cookiePathRewrite: {"*": ""}
 };
 const optionsAccessToken = {
   target: 'https://raindrop.io/oauth',
   changeOrigin: true,
   ws: false,
-  logLevel: 'info',
+  logLevel: 'debug',
   cookiePathRewrite: {"*": ""}
 };
 const optionsCollectionsAndRaindrop = {
   target: 'https://api.raindrop.io/rest/v1',
   changeOrigin: true,
   ws: false,
-  logLevel: 'info',
+  logLevel: 'debug',
   cookiePathRewrite: {"*": ""}
 };
 
 
 const exampleProxy = createProxyMiddleware(options);
 const proxyAccessToken = createProxyMiddleware(optionsAccessToken);
-const proxyCollectionsAndRaindrop = createProxyMiddleware(optionsCollectionsAndRaindrop);
+const proxyCollection = createProxyMiddleware(optionsAccessToken);
+const proxyCollections = createProxyMiddleware(optionsAccessToken);
+const proxyChildrens = createProxyMiddleware(optionsAccessToken);
+const proxyRaindrop = createProxyMiddleware(optionsAccessToken);
+const proxyRaindrops = createProxyMiddleware(optionsAccessToken);
+const proxyUser = createProxyMiddleware(optionsAccessToken);
+// const proxyCollectionsAndRaindrop = createProxyMiddleware(optionsCollectionsAndRaindrop);
 
 const app = express();
 app.use('/bookmarks', exampleProxy);
 app.use('/access_token', proxyAccessToken);
-app.use('/collection', proxyCollectionsAndRaindrop);
-app.use('/collections', proxyCollectionsAndRaindrop);
-app.use('/collections/childrens', proxyCollectionsAndRaindrop);
-app.use('/raindrop', proxyCollectionsAndRaindrop);
-app.use('/raindrops', proxyCollectionsAndRaindrop);
-app.use('/user', proxyCollectionsAndRaindrop);
+app.use('/collection', proxyCollection);
+app.use('/collections', proxyCollections);
+app.use('/collections/childrens', proxyChildrens);
+app.use('/raindrop', proxyRaindrop);
+app.use('/raindrops', proxyRaindrops);
+app.use('/user', proxyUser);
 app.use(cors());
 // Serve only the static files form the dist directory'./node_modules/http-proxy-middleware/lib/index'
 app.use(express.static('./dist/image-task-elilink'));
